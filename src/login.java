12d05df74928e7156570546f4ae5eaac73856c0c
Lucca <62717844+Lucca19@users.noreply.github.com>
2022-03-25 22:02:23 -0300

    imports

--- src/login.java
+++ src/login.java
@@ -1,8 +1,8 @@
-import javax.swing.JButton;
 import javax.swing.*;
 import java.awt.*;
-
-public class login {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class login extends JFrame{
     public static void main(String[] args) {
      JButton button = new JButton("Login");
         
