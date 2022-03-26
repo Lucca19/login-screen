d92445e3bbbb575239cf7eb8bfa8dad636126437
Lucca <62717844+Lucca19@users.noreply.github.com>
2022-03-25 21:55:53 -0300

    first commit

---
+++ src/login.java
@@ -0,0 +1,11 @@
+import javax.swing.JButton;
+import javax.swing.*;
+import java.awt.*;
+
+public class login {
+    public static void main(String[] args) {
+     JButton button = new JButton("Login");
+        
+    }
+
+}
