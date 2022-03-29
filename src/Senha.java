1966b34903858e7fb1a4cce07f4efcaebddcc1e6
Lucca <62717844+Lucca19@users.noreply.github.com>
2022-03-28 21:09:16 -0300

    DB connection -2

--- src/Senha.java
+++ src/Senha.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Senha {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Senha implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
---
+++ src/connect.java
@@ -0,0 +1,59 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class connect {
+
+    public static String status = "Não conectou...";
+
+    public connect() {   
+    }
+
+    public static java.sql.Connection getConexaoMySQL() {
+
+        Connection connection = null;
+
+        try {
+            String driverName = "com.mysql.jdbc.Driver";
+            
+            Class.forName(driverName);
+            String serverName = "127.0.0.1/3306";    //caminho do servidor do BD
+
+            String mydatabase ="unifil";        //nome do seu banco de dados
+    
+            String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
+    
+            String username = "root";
+    
+            String password = "rafaela1903";
+    
+            connection = DriverManager.getConnection(url, username, password);
+    
+    
+            if (connection != null) {
+                status = ("STATUS--->Conectado com sucesso!");
+            } else {
+                status = ("STATUS--->Não foi possivel realizar conexão");
+            }
+            return connection;
+            
+            }catch (ClassNotFoundException e) {
+
+                System.out.println("O driver expecificado nao foi encontrado.");
+    
+                return null;
+    
+            } catch (SQLException e) {
+
+    
+                System.out.println("Nao foi possivel conectar ao Banco de Dados.");
+    
+                return null;
+    
+            }
+    
+    
+    
+        }
+    
+        }}}
